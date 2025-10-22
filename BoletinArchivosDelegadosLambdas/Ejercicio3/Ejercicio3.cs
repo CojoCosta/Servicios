9b9dff66c21791e0c69271441261a6250da79730
CojoCosta <dcosta1882000@gmail.com>
2025-10-22 09:29:24 +0200

    changes

---
+++ BoletinArchivosDelegadosLambdas/Ejercicio3/Ejercicio3.cs
@@ -0,0 +1,45 @@
+﻿namespace Ejercicio3
+{
+    internal class Ejercicio3
+    {
+        double num;
+        public delegate double Operation(double x);
+
+        public static double pedirDouble()
+        {
+            double num;
+            Console.WriteLine("Dime un puto numero");
+            if (double.TryParse(Console.ReadLine(), out num))
+            {
+                return num;
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            double num = pedirDouble();
+            int numero = 0;
+            Console.WriteLine("Elija hacer el cuadrado o cubo(2 o 3)¿?");
+            while (numero != 2 && numero != 3)
+            {
+                Operation resultados;
+                bool flag = int.TryParse(Console.ReadLine(), out numero);
+                if (numero == 2)
+                {
+                    resultados = num => num * num;
+                    Console.WriteLine($"Resultado: {resultados(num)}");
+                }
+                else if (numero == 3)
+                {
+                    resultados = num => num * num * num;
+                    Console.WriteLine($"Resultado: {resultados(num)}");
+                }
+                else
+                {
+                    Console.WriteLine("Nº incorrecto");
+                }
+            }
+        }
+    }
+}
