7ecb92248d0547f2351c79242e7a9d972df9ea05
CojoCosta <dcosta1882000@gmail.com>
2025-10-24 13:41:37 +0200

    changes

--- Hilos/Ejemplo2/Program.cs
+++ Hilos/Ejemplo2/Program.cs
@@ -3,37 +3,44 @@
     internal class Program
     {
         static bool running = true;//Booleana compartida para controlar los bucles
+        static readonly object l = new object();
         static void Main(string[] args)
         {
-            Thread thread = new Thread(writeDown);
+            Thread thread = new Thread(WriteDown);
             thread.Start();
             //writeUp
             int i = 1;
             while (running)
             {
-                Console.SetCursorPosition(1, 1);
-                Console.Write("{0,4}", i);
-                i++;
-            Thread.Sleep(1000000);
-                if (i >= 1000)
+                lock (l)
                 {
-                    running = false;
+                    Console.SetCursorPosition(1, 1);
+                    Console.Write("{0,4}", i);
+                    i++;
+                    Thread.Sleep(100);
+                    if (i >= 1000)
+                    {
+                        running = false;
+                    }
                 }
             }
             Console.ReadKey();
         }
-        static void writeDown()
+        static void WriteDown()
         {
             int i = 1;
             while (running)
             {
-                Console.SetCursorPosition(1, 20);
-                Console.Write("{0,4}", i);
-                i++;
-                Thread.Sleep(1000000);
-                if (i >= 1000)
+                lock (l)
                 {
-                    running = false;
+                    Console.SetCursorPosition(1, 20);
+                    Console.Write("{0,4}", i);
+                    i++;
+                    Thread.Sleep(1000);
+                    if (i >= 1000)
+                    {
+                        running = false;
+                    }
                 }
             }
         }
