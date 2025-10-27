a85cfc0cd31f6932a274eefc595cb22af12865eb
CojoCosta <dcosta1882000@gmail.com>
2025-10-27 13:11:07 +0100

    changes

---
+++ Hilos/Ejemplo7(Task)/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ejemplo7_Task_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(148, 67);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(148, 133);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hilos/Ejemplo7(Task)/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo7_Task_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random g = new Random();
+            this.BackColor = Color.FromArgb(255, g.Next(256), g.Next(256), g.Next(256));
+        }
+        public int SlowFind(int num, int[] vector)
+        {
+            Random g = new Random();
+            int position;
+            do
+            {
+                position = g.Next(vector.Length); // Random search :-/
+            } while (num != vector[position]);
+            return position;
+        }
+        //public void Init()
+        //{
+        //    Random g = new Random();
+        //    int[] v = new int[100000000]; // Ojo, esto ocupa 400MB
+        //    v[g.Next(v.Length)] = 1;
+        //    Stopwatch time = new Stopwatch(); // Timing control, no necesario.
+        //    time.Start();
+        //    int result = SlowFind(1, v);
+        //    time.Stop();
+        //    Debug.WriteLine($"Time Elapsed: {time.ElapsedMilliseconds} ms.");
+        //    Debug.WriteLine("I couldn't do anything during the search :-(");
+        //    label1.Text = $"The 1 is in the position {result} of the vector.";
+        //}
+        public async void Init() // async ? “Puedo usar await dentro”
+        {
+            // Cada hilo crea su propia instancia de Random
+            // Evita problemas de thread-safety sin necesidad de locks
+            Random g = new Random();
+            // Código síncrono normal
+            int[] v = new int[100000000];
+            v[g.Next(v.Length)] = 1;
+            // Operación asíncrona - NO BLOQUEA
+            Task<int> task = Task.Run(() => SlowFind(1, v));
+            // Mientras se hace la operación previa, la UI responde.
+            Debug.WriteLine("Doing things while waiting for the result. :-D");
+            // await ? "espera sin bloquear, luego continúa aquí".
+            int result = await task;
+            label1.Text = $"The 1 is in the position {result} of the vector.";
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+    }
+}
