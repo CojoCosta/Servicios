15b1fb040f9d7433620f50af9233e99cc8e731e6
CojoCosta <dcosta1882000@gmail.com>
2025-10-27 09:45:34 +0100

    changes

---
+++ Hilos/Ejemplo4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ejemplo4
+{
+    internal class Program
+    {
+        static object l = new object();
+        static void Main(string[] args) // Hilo principal, foreground por defecto.
+        {
+            Thread thread = new Thread(writeDown);
+            thread.IsBackground = true; // Punto clave. Prueba a cambiarlo a false.
+            thread.Start();
+            for (int i = 1; i < 50; i++)
+            {
+                lock (l)
+                {
+                    Console.SetCursorPosition(1, 1);
+                    Console.Write("{0,4}", i);
+                }
+                Thread.Sleep(50);
+            }
+        } // Cuando acaba el programa se cierra interrumpiendo el hilo background.
+        static void writeDown()
+        {
+            for (int i = 1; i < 50; i++)
+            {
+                lock (l)
+                {
+                    Console.SetCursorPosition(1, 20);
+                    Console.Write("{0,4}", i);
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
