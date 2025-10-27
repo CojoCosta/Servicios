0ccc92d04b96bd189fa9aa9b1a1a3901f7a620e4
CojoCosta <dcosta1882000@gmail.com>
2025-10-27 12:29:14 +0100

    changes

---
+++ Hilos/Ejemplo5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejemplo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] hilos = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                hilos[i] = new Thread(writeALotOfChars);
+                char c = (char)('A' + i);
+                hilos[i].Start(c);
+            }
+            for (int i = 1; i < 1000; i++)
+            {
+                Console.Write("-Main-");
+            }
+            Console.ReadKey();
+        }
+        static void writeALotOfChars(object a)
+        {
+            for (int i = 1; i < 1000; i++)
+            {
+                Console.Write((char)a);
+            }
+        }
+    }
+}
