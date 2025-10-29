6d602d31fdb231c16c272e3b1266d42176dd2b2c
CojoCosta <dcosta1882000@gmail.com>
2025-10-29 11:18:33 +0100

    changes

---
+++ BoletinHilos/Ejercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static bool running = true;
+        public static void contadorSuma()
+        {
+            int i = 0;
+            while (running)
+            {
+                Console.SetCursorPosition(1, 1);
+                Console.Write(i);
+                i++;
+                if (i>=500)
+                {
+                    running = false;
+                }
+            }
+        }
+        public static void contadorResta()
+        {
+            int i = 500;
+            while (running)
+            {
+                Console.SetCursorPosition(1, 20);
+                Console.Write(i);
+                i--;
+                if (i <= 0)
+                {
+                    running = false;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread suma = new Thread(contadorSuma);
+            Thread resta = new Thread(contadorResta);
+           
+            resta.Start();
+            suma.Start();
+        }
+    }
+}
