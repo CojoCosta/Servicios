8acc54b7db9fe85f158fc5e5ba17cf1ea78f714f
CojoCosta <dcosta1882000@gmail.com>
2025-11-07 12:53:13 +0100

    changes

--- BoletinHilos/Ejercicio2/Program.cs
+++ BoletinHilos/Ejercicio2/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            Thread[] caballos = new Thread[5];
+            Console.WriteLine("~~~~BIENVENIDO AL HIPÓDROMO VIVAS~~~~");
+            Console.WriteLine("Elija el caballo por el que apuesta (1 -5)");
+            for (int i = 0; i < caballos.Length; i++)
+            {
+                caballos[i].Start();
+            }
+
         }
     }
 }
