08e361c16303e99bbc6a164cd44db868a989c88a
CojoCosta <diegocostapereira18@gmail.com>
2025-11-23 19:10:41 +0100

    changes 23/11

---
+++ BoletinHilos/Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnBusqueda = new System.Windows.Forms.Button();
+            this.btnPosicion = new System.Windows.Forms.Button();
+            this.btnHttp = new System.Windows.Forms.Button();
+            this.txtComun = new System.Windows.Forms.TextBox();
+            this.listaBusqueda = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(55, 43);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(678, 26);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnBusqueda
+            // 
+            this.btnBusqueda.Location = new System.Drawing.Point(55, 193);
+            this.btnBusqueda.Name = "btnBusqueda";
+            this.btnBusqueda.Size = new System.Drawing.Size(120, 47);
+            this.btnBusqueda.TabIndex = 1;
+            this.btnBusqueda.Text = "Búsqueda";
+            this.btnBusqueda.UseVisualStyleBackColor = true;
+            this.btnBusqueda.Click += new System.EventHandler(this.btnBusqueda_click);
+            // 
+            // btnPosicion
+            // 
+            this.btnPosicion.Location = new System.Drawing.Point(319, 193);
+            this.btnPosicion.Name = "btnPosicion";
+            this.btnPosicion.Size = new System.Drawing.Size(120, 47);
+            this.btnPosicion.TabIndex = 2;
+            this.btnPosicion.Text = "Posición";
+            this.btnPosicion.UseVisualStyleBackColor = true;
+            this.btnPosicion.Click += new System.EventHandler(this.btnPosicion_Click);
+            // 
+            // btnHttp
+            // 
+            this.btnHttp.Location = new System.Drawing.Point(613, 193);
+            this.btnHttp.Name = "btnHttp";
+            this.btnHttp.Size = new System.Drawing.Size(120, 47);
+            this.btnHttp.TabIndex = 3;
+            this.btnHttp.Text = "Http";
+            this.btnHttp.UseVisualStyleBackColor = true;
+            this.btnHttp.Click += new System.EventHandler(this.btnHttp_Click);
+            // 
+            // txtComun
+            // 
+            this.txtComun.Location = new System.Drawing.Point(55, 161);
+            this.txtComun.Name = "txtComun";
+            this.txtComun.Size = new System.Drawing.Size(678, 26);
+            this.txtComun.TabIndex = 4;
+            // 
+            // listaBusqueda
+            // 
+            this.listaBusqueda.FormattingEnabled = true;
+            this.listaBusqueda.ItemHeight = 20;
+            this.listaBusqueda.Location = new System.Drawing.Point(55, 257);
+            this.listaBusqueda.Name = "listaBusqueda";
+            this.listaBusqueda.Size = new System.Drawing.Size(218, 244);
+            this.listaBusqueda.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 513);
+            this.Controls.Add(this.listaBusqueda);
+            this.Controls.Add(this.txtComun);
+            this.Controls.Add(this.btnHttp);
+            this.Controls.Add(this.btnPosicion);
+            this.Controls.Add(this.btnBusqueda);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnBusqueda;
+        private System.Windows.Forms.Button btnPosicion;
+        private System.Windows.Forms.Button btnHttp;
+        private System.Windows.Forms.TextBox txtComun;
+        private System.Windows.Forms.ListBox listaBusqueda;
+    }
+}
+
---
+++ BoletinHilos/Ejercicio4/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string buscaPalabra(string dirección, string palabraBuscada)
+        {
+            int contador = 0;
+            using (StreamReader sr = new StreamReader(dirección))
+            {
+                string textoArchivo = sr.ReadToEnd();
+                for (int i = 0; i < (textoArchivo.Length - palabraBuscada.Length); i++)
+                {
+                    if (textoArchivo.Substring(i, palabraBuscada.Length) == palabraBuscada)
+                    {
+                        contador++;
+                    }
+                }
+            }
+            return $"Archivo:{Path.GetFileName(dirección)}, {palabraBuscada}, {contador}";
+        }
+
+        List<Task<String>> tareasBusqueda = new List<Task<String>>();
+        private async void btnBusqueda_click(object sender, EventArgs e)
+        {
+            DirectoryInfo directorioActual = new DirectoryInfo(txtUrl.Text);
+            foreach (FileInfo archivo in directorioActual.GetFiles())
+            {
+                if (archivo.Extension == ".txt")
+                {
+                    tareasBusqueda.Add(Task.Run(() => buscaPalabra(archivo.FullName, txtComun.Text)));
+                }
+            }
+            while (tareasBusqueda.Count > 0)
+            {
+                Task<string> tareaAcabada = await Task.WhenAny(tareasBusqueda);
+                tareasBusqueda.Remove(tareaAcabada);
+                listaBusqueda.Items.Add(tareaAcabada.Result);
+            }
+        }
+
+        public string primeraPosicionPalabra(string ruta, string palabraBuscada)
+        {
+            using (StreamReader sr = new StreamReader(ruta))
+            {
+                string textoArchivo = sr.ReadToEnd();
+                for (int i = 0; i < textoArchivo.Length; i++)
+                {
+                    if (textoArchivo.Substring(i, palabraBuscada.Length) == palabraBuscada)
+                    {
+                        return $"{Path.GetFileName(ruta)}, {palabraBuscada}, {i}";
+                    }
+                }
+            }
+            return $"{Path.GetFileName(ruta)}, {palabraBuscada}, -1";
+        }
+
+        List<Task<String>> tareasPosicion = new List<Task<String>>();
+        private async void btnPosicion_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo directorioActual = new DirectoryInfo(txtUrl.Text);
+            foreach (FileInfo archivo in directorioActual.GetFiles())
+            {
+                if (archivo.Extension == ".txt")
+                {
+                    tareasPosicion.Add(Task.Run(() => primeraPosicionPalabra(archivo.FullName, txtComun.Text)));
+                }
+            }
+
+            string[] tareasRealizadas = await Task.WhenAll(tareasPosicion);
+            while (tareasPosicion.Count > 0)
+            {
+                foreach (string tareaRealizada in tareasRealizadas)
+                {
+                    listaBusqueda.Items.Add(tareaRealizada);
+                }
+            }
+            tareasPosicion.Clear();
+        }
+
+        private async void btnHttp_Click(object sender, EventArgs e)
+        {
+            HttpClient http = new HttpClient();
+            try
+            {
+                Task<HttpResponseMessage> tareaPorRealizar = http.GetAsync(txtComun.Text);
+                listaBusqueda.Items.Add(await tareaPorRealizar);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
