240a093ec107e398ca37b1492f0746a561b05098
CojoCosta <diegocostapereira18@gmail.com>
2025-11-23 18:06:03 +0100

    changes 22/11

---
+++ BoletinHilos/Ejercicio3/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnDesc = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(86, 162);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(189, 26);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnDesc
+            // 
+            this.btnDesc.Location = new System.Drawing.Point(86, 225);
+            this.btnDesc.Name = "btnDesc";
+            this.btnDesc.Size = new System.Drawing.Size(189, 28);
+            this.btnDesc.TabIndex = 1;
+            this.btnDesc.Text = "Descargar";
+            this.btnDesc.UseVisualStyleBackColor = true;
+            this.btnDesc.Click += new System.EventHandler(this.btnDesc_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(356, 46);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(432, 324);
+            this.txtResults.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnDesc);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "Form1";
+            this.Text = "Tasks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnDesc;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ BoletinHilos/Ejercicio3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AcceptButton = btnDesc;
+        }
+        async Task<string> DownloadFileAsync(string fileName, int delayMs) //devolvemos el string
+        { 
+            await Task.Delay(delayMs);
+            return $"File {fileName} downloaded in {delayMs} ms";
+        }
+        private async void btnDesc_Click(object sender, EventArgs e)
+        {
+            int aleatorio = nRandom(10);
+            //texto es un string que al hacer el await de la tarea lo estamos igualando al resultado de ejecutar dicha tarea, si no hicieramos await no devolveria el string ya que devolveria la propia tarea (Task) sin completar pq el programa seguiria ejecutandose, de esta manera le mandamos esperar a acabar la tarea y obtenemos su resultado
+            string resultadoTask = await DownloadFileAsync(txtFileName.Text, aleatorio);
+            txtResults.Text += resultadoTask + Environment.NewLine;
+        }
+        Random rd = new Random();
+        public int nRandom(int numMax)
+        {
+            return (int)rd.Next(numMax);
+        }
+
+    }
+}
