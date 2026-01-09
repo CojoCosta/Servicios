00843ce5788d78d00c430532cf5a39b9c6fc0321
CojoCosta <dcosta1882000@gmail.com>
2026-01-09 13:57:13 +0100

    changes 09/01

--- BoletinNetWorking/Ejercicio1/Program.cs
+++ BoletinNetWorking/Ejercicio1/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace Ejercicio1
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Ejercicio1
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public bool ServerRunning { set; get; } = true;
+        public int Port { get; set; } = 31416;
+        public void InitServer()
         {
-            Console.WriteLine("Hello, World!");
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, Port);
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                s.Bind(ie);
+                s.Listen(10);
+                Console.WriteLine($"Servidor iniciado. " + $"Escuchando en {ie.Address}:{ie.Port}");
+                Console.WriteLine("Esperando conexiones... (Ctrl+C para salir)");
+                try
+                {
+                    while (ServerRunning)
+                    {
+                        Socket cliente = s.Accept();
+                        Thread hilo = new Thread(() => ProtocoloCliente(cliente));
+                        hilo.Start();
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine("Servidor cerrado");
+                }
+            }
+        }
+        public void ProtocoloCliente(Socket sCliente)
+        {
+            using (sCliente)
+            {
+                IPEndPoint ieCliente = (IPEndPoint)sCliente.RemoteEndPoint;
+                Console.WriteLine($"Cliente conectado:{ieCliente.Address} " + $"en puerto {ieCliente.Port}");
+                Encoding codificacion = Console.OutputEncoding;
+                using (NetworkStream ns = new NetworkStream(sCliente))
+                using (StreamReader sr = new StreamReader(ns)
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    sw.AutoFlush = true;
+                    string welcome = "Welcome to my server";
+                    sw.WriteLine(welcome);
+                    string? msg = "";
+                    try
+                    {
+                        if (msg != null)
+                        {
+                            if (msg != "time" || msg != "date" || msg != "date" || msg != "close")
+                            {
+                                Console.WriteLine($"Error de comando: {msg}");
+                            }
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        msg = null;
+                    }
+                    Console.WriteLine("Cliente desconectado.\nConexión cerrada");
+                }
+            }
         }
     }
 }
